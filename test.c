20f14ac4450d299c1fde177fe7b79bca6d197432
moon <71251388+moonliii@users.noreply.github.com>
2021-11-13 00:51:13 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,12 @@
+int main(){
+        int a,b,c;
+        int flag = 1;
+        if(flag==1){
+                c=a+b;
+        }
+        else {
+                c=a-b;
+        }
+
+        return 0;
+}
